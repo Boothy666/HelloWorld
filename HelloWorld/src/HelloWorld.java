08fcc8e081d852ebe814111d8af0880fbbf523f7
Boothy666 <mark_boothroyd@hotmail.com>
2015-10-09 12:17:48 +0100

    New comment added as test

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Added a new comment
+		// 2nd comment added
 		System.out.println("Hello world!");
 	}
 

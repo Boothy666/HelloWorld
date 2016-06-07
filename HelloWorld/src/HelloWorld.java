2f9d5e9a6fd520f016a374d0644b60a236396800
Boothy666 <mark_boothroyd@hotmail.com>
2016-06-07 12:28:38 +0100

    Added javadocs

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,6 +1,9 @@
 
 public class HelloWorld {
-
+	/** Simple Hello World java Program
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// A comment
 		System.out.println("Hello world!");
